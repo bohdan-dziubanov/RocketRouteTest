39161a61c62b70dbfa77859b1188cccdceacc0f2
Bohdan Dziubanov <bohdan.dziubanov@oracle.com>
2016-11-15 15:03:52 +0200

    test2

--- templates/layouts/header.php
+++ templates/layouts/header.php
@@ -4,7 +4,7 @@
         <meta http-equiv="Content-type" content="text/html; charset=utf-8">
         <title><?php echo $title ?></title>
         <?php $protocol = !empty($_SERVER['HTTPS']) ? 'https' : 'http'; ?>
-        <link rel="stylesheet" href="<?php echo $protocol?>://<?php echo $_SERVER['HTTP_HOST']?>/resources/css/styles.css" type="text/css">
+        <link rel="stylesheet" href="resources/css/styles.css" type="text/css">
         <script src="resources/js/maps.js"></script>
         <script async defer
             src="https://maps.googleapis.com/maps/api/js?key= AIzaSyDzvOzADLpCkfmw6LhK6K6i6FUzc7z4E0I&callback=initMap">
