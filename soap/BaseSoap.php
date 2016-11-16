0660fe3a875386c37e8d2fa7608b4f6e2fa0e0f5
bdziuban <bdziuban>
2016-11-16 14:24:02 +0200

    parse geo data

--- soap/BaseSoap.php
+++ soap/BaseSoap.php
@@ -5,7 +5,7 @@ namespace soap;
 abstract class BaseSoap
 {
     protected $usr = 'bohdan.dziubanov@gmail.com';
-    protected $pass = '3b239f8dd0a3ed058dde1792254144c8';
+    protected $pass = 'F8kPPaRbQZqjWTzzrbaR';
     protected $md5key = '4GnRqmDpNH3PHuPLmZLS';
     protected $authUrl = 'https://flydev.rocketroute.com/remote/auth';
     protected $token;
