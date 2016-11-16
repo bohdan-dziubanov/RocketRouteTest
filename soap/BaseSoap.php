3ce4ca719eb933e8bf755b5d7b70ca1cbf295a19
bdziuban <bdziuban>
2016-11-16 14:58:18 +0200

    parse geo data

--- soap/BaseSoap.php
+++ soap/BaseSoap.php
@@ -45,7 +45,7 @@ abstract class BaseSoap
         {
             $resultUsXml = new \SimpleXMLElement($response);
             $resultAsArray = json_decode(json_encode($resultUsXml), TRUE);
-var_dump(htmlspecialchars($req));exit;
+var_dump(htmlspecialchars($req, ENT_XML1));exit;
             if (empty($resultAsArray['KEY']))
             {
                 throw new \Exception('Unauthorized', 401);
